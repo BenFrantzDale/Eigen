82a2d1a3a1f25b5cc67f160d02375e9898f249fa
Ben FrantzDale <ben@formlabs.com>
2019-09-18 14:43:04 -0400

    C++17

--- Eigen/src/Core/Functors.h
+++ Eigen/src/Core/Functors.h
@@ -969,14 +969,14 @@ template<typename T>
 struct functor_traits<std::not_equal_to<T> >
 { enum { Cost = 1, PacketAccess = false }; };
 
-template<typename T>
+/*template<typename T>
 struct functor_traits<std::binder2nd<T> >
 { enum { Cost = functor_traits<T>::Cost, PacketAccess = false }; };
 
 template<typename T>
 struct functor_traits<std::binder1st<T> >
 { enum { Cost = functor_traits<T>::Cost, PacketAccess = false }; };
-
+*/
 template<typename T>
 struct functor_traits<std::unary_negate<T> >
 { enum { Cost = 1 + functor_traits<T>::Cost, PacketAccess = false }; };
